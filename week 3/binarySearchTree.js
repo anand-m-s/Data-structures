377cfa8667537d44c365b4faaf361208d1304abb
anandms <anandsathyan19@gmail.com>
2024-01-31 16:14:24 +0530

    preOrder

--- week 3/binarySearchTree.js
+++ week 3/binarySearchTree.js
@@ -5,7 +5,7 @@ class Node{
         this.right = null
     }
 }
-    
+
 class BinarySearchTree{
     constructor(){
         this.root = null
@@ -51,14 +51,25 @@ class BinarySearchTree{
             return this.search(root.right,value)
         }
     }
+
+    preOrder(root){
+        if(root){
+            console.log(root.value)
+            this.preOrder(root.left)
+            this.preOrder(root.right)
+        }
+    }
 }
 
 const bst = new BinarySearchTree()
 bst.insert(10)
 bst.insert(5)
 bst.insert(15)
+bst.insert(3)
+bst.insert(7)
 console.log(bst.search(bst.root,10))
 console.log(bst.search(bst.root,5))
 console.log(bst.search(bst.root,15))
 console.log(bst.search(bst.root,20))
+bst.preOrder(bst.root)
 // console.log('bst is empty', bst.isEmpty());
